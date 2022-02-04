e5e2f9bc37b347ea687858c81d0d058d5d32ba3a
surgiholic <surgiholic@users.noreply.github.com>
2022-02-04 14:22:45 +0000

    deploy: 722287eec7990eb53fe5ed70cfe20336e82848f3

---
+++ _next/static/Mw58lMB-MDfPlac6rIGHT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/150-578b016225a87944.js","static/chunks/pages/index-13ff57f6d33558ad.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/YxSODYDflqN3852vZQGNG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/150-578b016225a87944.js","static/chunks/pages/index-7902ac642e5c7805.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
